de71f9ef55d6fc647ee3794487b6029b22e28ae5
Eduardo Augusto <eduardo.dos.santos@bradesco.com.br>
2025-08-10 16:15:04 -0300

    abstrata

---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,14 @@
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return largura * altura;
+    }
+}
