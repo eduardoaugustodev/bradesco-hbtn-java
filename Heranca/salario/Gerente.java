6f30e8975b34dd2534ebd7da72ec93d7db567f65
Eduardo Augusto <eduardo.dos.santos@bradesco.com.br>
2025-08-10 15:53:29 -0300

    Correção método

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -5,9 +5,8 @@ public class Gerente extends Empregado {
 
     @Override
     public double calcularBonus(Departamento departamento) {
-        // Exemplo: bônus de 20% do salário fixo se meta atingida, senão 0
         if (departamento.alcancouMeta()) {
-            return getSalarioFixo() * 0.2;
+            return Math.ceil(getSalarioFixo() * 0.2);
         } else {
             return 0.0;
         }
